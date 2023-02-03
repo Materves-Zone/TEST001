a8515569796d201af35462edf97714c0736304e1
CGIHUANG001 <cgihuang@sina.com>
2023-02-04 00:45:25 +0800

    20230204

---
+++ Shared/Hy_Assets/T_FlashTesting.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_FlashTesting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FlashPosNbInit();
+        FlashTestingPosInit();
+        FlashExpNbInit();
+        FlashTestingExpInit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject[] Pos;
+    public bool IsOnlyShow = false;
+    public bool IsFlashTasting = false;
+
+    private void FlashPosNbInit()
+    {
+
+    }
+    private void FlashPosNbStart()
+    {
+
+    }
+    private void FlashPosNbUpdate()
+    {
+
+    }
+    private void FlashPosNbResry()
+    {
+
+    }
+
+    private void FlashTestingPosInit()
+    {
+        IsFlashTasting = false;
+        for (int i = 0; i < Pos.Length; i++)
+        {
+            Pos[i].GetComponent<T_FlashControl>().IsFlash = false;
+            Pos[i].SetActive(false);
+        }
+    }
+    private void FlashTestingPosStart()
+    {
+        IsFlashTasting = true;
+        Pos[0].SetActive(true);
+        Pos[0].GetComponent<T_FlashControl>().IsFlash = true;
+    }
+    private void FlashTestingPosUpdate(int id)
+    {
+        if(IsOnlyShow)
+        {
+            for (int i = 0; i < Pos.Length; i++)
+            {
+                Pos[i].SetActive(false);
+                if (i==id)
+                {
+                    Pos[i].SetActive(true);
+                    Pos[i].GetComponent<T_FlashControl>().IsFlash = true;
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < Pos.Length; i++)
+            {
+                Pos[i].GetComponent<T_FlashControl>().IsFlash = false;
+                if (i == id)
+                {
+                    Pos[i].GetComponent<T_FlashControl>().IsFlash = true;
+                }
+            }
+        }
+    }
+    private void FlashTestingPosReset()
+    {
+        FlashTestingPosInit();
+    }
+
+    private void FlashExpNbInit()
+    {
+
+    }
+    private void FlashExpNbStart()
+    {
+
+    }
+    private void FlashExpNbUpdate()
+    {
+
+    }
+    private void FlashExpNbReset()
+    {
+
+    }
+
+    private void FlashTestingExpInit()
+    {
+
+    }
+    private void FlashTestingExpStart()
+    {
+
+    }
+    private void FlashTestingExpUpdate()
+    {
+
+    }
+    private void FlashTestingExpReset()
+    {
+
+    }
+}
---
+++ Shared/Hy_Assets/T_UserCheck.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_UserCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UserCheckInit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject Manager;
+    public T_FlashTesting t_FlashTesting;
+    private int CheckID;
+
+    private void UserCheckInit()
+    {
+        CheckID = 0;
+        t_FlashTesting = Manager.GetComponent<T_FlashTesting>();
+    }
+    private void UserCheckStart()
+    {
+        CheckID = 0;
+    }
+    private void UserCheckUpdate()
+    {
+
+    }
+    private void UserCheckReset()
+    {
+        UserCheckInit();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "FlashPos" && t_FlashTesting.IsFlashTasting)
+        {
+            
+        }
+    }
+}
