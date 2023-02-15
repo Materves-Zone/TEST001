e109c8802e06bc7f1af52eacbc805ba80c7c0eb9
Materves-Zone <90205716+Materves-Zone@users.noreply.github.com>
2023-02-15 19:28:51 +0800

    20230215_v1.0

--- Shared/Hy_Assets/Code/F_StaffSettings.cs
+++ Shared/Hy_Assets/Code/F_StaffSettings.cs
@@ -19,11 +19,38 @@ public class F_StaffSettings : MonoBehaviour
     public F_FlashTesting f_FlashTesting;
     public F_TTSTesting f_TTSTesting;
     public F_AvatarTesting f_AvatarTesting;
+    public GameObject[] ShowHideObjs;
+    private int ClickID;
 
     public void AllReseting()
     {
         f_FlashTesting.AllTestingReset();
         f_TTSTesting.AllTestingReset();
         f_AvatarTesting.AllTestingReset();
+    }
+
+    public void ShowHideSettings ()
+    {
+        switch (ClickID)
+        {
+            case 0:
+                ClickID++;
+                // Show
+                for (int i = 0; i < ShowHideObjs.Length; i++)
+                {
+                    ShowHideObjs[i].SetActive(true);
+                }
+                // Drag enable ...
+                break;
+
+            case 1:
+                // Hide
+                for (int i = 0; i < ShowHideObjs.Length; i++)
+                {
+                    ShowHideObjs[i].SetActive(false);
+                }
+                // Drag isable ...
+                break;
+        }
     }
 }
